06afc828ddf69878c97adc6c811cfa66994ab819
laspi <pro43071919@gmail.com>
2024-01-05 02:19:24 +0000

    implementation de la logique de connexion

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function create()
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+        ]);
+
+        $user = new User();
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = Hash::make($request->input('password'));
+        $user->save();
+
+        return response()->json(['message' => 'User registered successfully'], 200);
+    }
+}
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,29 @@
+<!-- resources/views/auth/register.blade.php -->
+
+<form method="POST" action="{{ route('register') }}">
+    @csrf
+
+    <div>
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name" value="{{ old('name') }}" required autofocus>
+    </div>
+
+    <div>
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" value="{{ old('email') }}" required>
+    </div>
+
+    <div>
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" required>
+    </div>
+
+    <div>
+        <label for="password_confirmation">Confirm Password</label>
+        <input type="password" name="password_confirmation" id="password_confirmation" required>
+    </div>
+
+    <div>
+        <button type="submit">Register</button>
+    </div>
+</form>
